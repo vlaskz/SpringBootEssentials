002a6fa9faf58d0e28a7c3299bb086a75232cdc3
Vlaskz <sam.velasquez@hotmail.com>
2021-06-25 00:53:52 -0300

    added Lombok and created log() function

---
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -0,0 +1,27 @@
+package academy.devdojo.springboot2.controller;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Log4j2
+@RestController
+@RequestMapping("anime")
+public class AnimeController {
+
+    @Autowired
+    private DateUtil dateUtil;
+
+    @GetMapping(path = "list")
+    public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Berserk"), new Anime("Hinokakera"));
+    }
+}
---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,19 @@
+package academy.devdojo.springboot2.domain;
+
+public class Anime {
+    private String name;
+    public Anime(String name){
+         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Anime(){
+    }
+}
